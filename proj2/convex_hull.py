0fb2b094f525f736e9763d5fd2c42e979b528a5a
Dat Vu <datvu4749@gmail.com>
2018-10-02 21:57:21 -0600

    finished raw with comments

--- proj2/convex_hull.py
+++ proj2/convex_hull.py
@@ -39,7 +39,7 @@ class ConvexHullSolverThread(QThread):
 			# if (p.y() == up_y.y()) & (p.x() == up_y.x()):
 			# 	continue
 			# angle = (float)( ( up_y.y() - p.y() ) / ( up_y.x() - p.x() ) )
-			print(p)
+			print(str(myList.index(p)) + " - " + str(p))
 			# print(angle)
 
 	def drawFromTop(self, lst, top):
@@ -52,7 +52,10 @@ class ConvexHullSolverThread(QThread):
 			result = lst
 		result = self.makeHull(lst)
 
-		polygon = [QLineF( lst[i], lst[(i+1)%len(lst)]) for i in range(len(lst))]
+		print("\n\n\nFINISHED solving.\nEnding result goes in order:")
+		self.printPoints(result)
+
+		polygon = [QLineF( result[i], result[(i+1)%len(result)]) for i in range(len(result))]
 		assert( type(polygon) == list and type(polygon[0]) == QLineF )
 		self.show_hull.emit(polygon,(255,0,0))
 
@@ -65,65 +68,191 @@ class ConvexHullSolverThread(QThread):
 		RHS = self.makeHull(lst[cut:len(lst)])
 		return self.mergeHulls(LHS,RHS)
 
+	# def ff(self, lst, )
+
 	def mergeHulls(self, LHS, RHS):
-		print("\nLHS = " + str(LHS))
-		print("\nRHS = " + str(RHS))
+		# print("\nLHS = " + str(LHS))
+		# print("\nRHS = " + str(RHS))
 		if len(LHS) == 1 & len(RHS) == 1:
 			result = LHS + RHS
+			return result
 		elif ( (len(LHS) == 1) & (len(RHS) == 2) ) | ( (len(LHS) == 2) & (len(RHS) == 1) ):
 			result = LHS + RHS
 			result = self.sortClockwise(result)
+			return result
 		else:
-			LHS_right = self.findRightMost(LHS)
-			RHS_left = self.findLeftMost(RHS)
-			print("LHS_right = " + str(LHS_right) + " with index " + str(LHS.index(LHS_right)))
-			print("RHS_left = " + str(RHS_left) + " with index " + str(RHS.index(RHS_left)))
-			a,b = self.findUpperTangent(LHS,RHS,LHS_right, RHS_left)
-
-			print("\n\n\n")
+			print("\n\nSTARTING mergeHulls. The two sides are")
+			print("LHS = " + str(LHS) + " OF SIZE " + str(len(LHS)))
+			print("RHS = " + str(RHS) + " OF SIZE " + str(len(RHS)))
 
 
+			LHS_up,RHS_up = self.findUpperTangent(LHS,RHS,self.findRightMost(LHS), self.findLeftMost(RHS))
+			print("tangent upper include points:")
+			print(str(LHS_up) + " index = " + str(LHS.index(LHS_up)))
+			print(str(RHS_up) + " index = " + str(RHS.index(RHS_up)))
+			print("\n")
 
-			result = LHS + RHS
+			LHS_down,RHS_down = self.findLowerTangent(LHS,RHS,self.findRightMost(LHS), self.findLeftMost(RHS))
+			print("tangent Lower include points:")
+			print(str(LHS_down) + " index = " + str(LHS.index(LHS_down)))
+			print(str(RHS_down) + " index = " + str(RHS.index(RHS_down)))
+			print("\n")
 
-		print("result now = " + str(result))
+			#TODO: merge with found points
+			if LHS.index(LHS_down) > LHS.index(LHS_up):
+				print("\tLHS_down index > LHS_up index (Up comes before Down)\n\t=>> current = [0 -> up -> down -> end].\n\t\t[down:0:up]\n")
+				print("\t\t[down:0] = " + str( LHS[ LHS.index(LHS_down):len(LHS) ]))
+				print("\t\t[0:up] = " + str( LHS[ 0:LHS.index(LHS_up)+1 ] ) )
+				leftHull = LHS[ LHS.index(LHS_down):len(LHS) ] + LHS[ 0:LHS.index(LHS_up)+1]
+			else:
+				print("\tLHS_down index < LHS_up index (Down comes before Up)\n\t=>> current = [0 -> down -> up -> end].\n\t\t[down:up]\n")
+				print("\t\t[down:up] = " + str(LHS[ LHS.index(LHS_down):LHS.index(LHS_up)+1 ]))
+				leftHull = LHS[ LHS.index(LHS_down):LHS.index(LHS_up)+1 ]
+
+
+			if RHS.index(RHS_up) > RHS.index(RHS_down):
+				print("\tRHS_up index > RHS_down index (Down comes before Up)\n\t=>> current = [0 -> down -> up -> end].\n\t\t[up:0:down]\n")
+				print("\t\t[up:0] = " + str(RHS[ RHS.index(RHS_up):len(RHS) ]))
+				print("\t\t[0:down] = " + str(RHS[ 0:RHS.index(RHS_down)+1 ]) )
+				rightHull = RHS[ RHS.index(RHS_up):len(RHS) ] + RHS[ 0:RHS.index(RHS_down)+1]
+			else:
+				print("\tRHS_up index < RHS_down index (Up comes before Down)\n\t=>> current = [0 -> up -> down -> end].\n\t\t[up:down]\n")
+				print("\t\t[up:down] = " + str( RHS[ RHS.index(RHS_up):RHS.index(RHS_down)+1 ] ) )
+				rightHull = RHS[ RHS.index(RHS_up):RHS.index(RHS_down)+1 ]
+
+			print("leftHull = " + str(leftHull))
+			print("rightHull = " + str(rightHull))
+
+			result = leftHull + rightHull
+
+		print("\nEnding mergeHulls. Result = " + str(result) + "\n\n\n\n\n\n\n")
 		return result
 
 	def findUpperTangent(self, LHS, RHS, LHS_right, RHS_left):
 		cont = True
-		amax = -100
 
-
-		LHS = LHS[::-1]
-		print("LHS = " + str(LHS))
-		print("RHS = " + str(RHS))
+		LHS = LHS[::-1] + LHS[::-1]
+		RHS = RHS + RHS
+		# print("\n\n\n\nPREPARE TO FIND UPPER TANGENT\n")
+		# print("LHS = " + str(LHS))
+		# print("RHS = " + str(RHS))
 		#set current points of tangent, starting with leftmost and rightmost
 		LHS_current = LHS_right
 		RHS_current = RHS_left
 
 		while cont == True:
+			# print("\n\n\n\n\n\nINTO CONT in UPPER TANGENT")
+			continueRHS = True
+			continueLHS = True
 
-			# cont_Right = True
-			# cont_Left = True
 
-			for i in range( RHS.index(RHS_current), len(RHS)+RHS.index(RHS_current) ):
-				prev_angle = 0
+			for i in range( RHS.index(RHS_current) + 1, len(RHS)): #index of first occurence
+				prev_angle = self.findSlope( LHS_current, RHS_current)
 				# get slope of current on LHS and next on RHS
-				angle = self.findSlope( LHS_current, RHS[i+1] )
+				angle = self.findSlope( LHS_current, RHS[i] )
+				# print("connect LHS right -> RHS at i = " + str(i))
+				# print("prev_angle = " + str(prev_angle))
+				# print("current angle = " + str(angle) + "\n\tbetween:\n\t\t" + str(LHS_current) + " and\n\t\t" + str(RHS[i]))
+				# print( str(angle > prev_angle) + "\n\n")
 
-				if angle > prev_angle: #angle increases					
+				if angle > prev_angle: #angle increases
 					RHS_current = RHS[i]
 					prev_angle = angle
+					continueLHS = True
+					# print("RHS_current CHANGED: now is = " + str(RHS_current) + "\n")
 				else:
-					cont = False
+					# print("RHS no longer changing\n")
+					continueRHS = False
 					break
 
-			for i in range( LHS.index(LHS_current), len(LHS)+LHS.index(LHS_current) ):
+			for i in range( LHS.index(LHS_current) + 1, len(LHS) ):
+				prev_angle = self.findSlope( LHS_current, RHS_current)
+				# get slope of current on LHS and next on RHS
 				angle = self.findSlope( RHS_current, LHS[i] )
-				if
+				# print("connect RHS left -> LHS")
+				# print("prev_angle = " + str(prev_angle))
+				# print("current angle = " + str(angle))
+
+				if angle < prev_angle:
+					LHS_current = LHS[i]
+					prev_angle = angle
+					continueRHS = True
+					# print("LHS_current CHANGED: now is = " + str(LHS_current) + "\n")
+				else:
+					# print("LHS no longer changing\n")
+					continueLHS = False
+					break
+
+			cont = continueLHS | continueRHS
+
+			# print("cont now is " + str(cont))
 
 		return LHS_current, RHS_current
 
+	def findLowerTangent(self, LHS, RHS, LHS_right, RHS_left):
+		cont = True
+
+		RHS = RHS[::-1] + RHS[::-1]
+		LHS = LHS + LHS
+		# print("\n\n\n\nPREPARE TO FIND LOWER TANGENT\n")
+		# print("LHS = " + str(LHS))
+		# print("RHS = " + str(RHS))
+		#set current points of tangent, starting with leftmost and rightmost
+		LHS_current = LHS_right
+		RHS_current = RHS_left
+
+		while cont == True:
+			# print("\n\n\n\n\n\nINTO CONT in LOWER TANGENT")
+			continueRHS = True
+			continueLHS = True
+
+
+			for i in range( RHS.index(RHS_current) + 1, len(RHS)): #index of first occurence
+				prev_angle = self.findSlope( LHS_current, RHS_current)
+				# get slope of current on LHS and next on RHS
+				angle = self.findSlope( LHS_current, RHS[i] )
+				# print("connect LHS right -> RHS at i = " + str(i))
+				# print("prev_angle = " + str(prev_angle))
+				# print("current angle = " + str(angle) + "\n\tbetween:\n\t\t" + str(LHS_current) + " and\n\t\t" + str(RHS[i]))
+				# print( str(angle < prev_angle) + "\n\n")
+
+
+				if angle < prev_angle: #angle increases
+					RHS_current = RHS[i]
+					prev_angle = angle
+					continueLHS = True
+					# print("RHS_current CHANGED: now is = " + str(RHS_current) + "\n")
+				else:
+					# print("RHS no longer changing\n")
+					continueRHS = False
+					# if i == RHS.index(RHS_current) + 1:
+					# 	print("NO CHANGE!! BREAKING OUT of cont. FOUND \n")
+					# 	cont = False
+					break
+
+			for i in range( LHS.index(LHS_current) + 1, len(LHS) ):
+				prev_angle = self.findSlope( LHS_current, RHS_current)
+				# get slope of current on LHS and next on RHS
+				angle = self.findSlope( RHS_current, LHS[i] )
+				# print("connect RHS left -> LHS")
+				# print("prev_angle = " + str(prev_angle))
+				# print("current angle = " + str(angle))
+
+				if angle > prev_angle:
+					LHS_current = LHS[i]
+					prev_angle = angle
+					continueRHS = True
+					# print("LHS_current CHANGED: now is = " + str(LHS_current) + "\n")
+				else:
+					# print("LHS no longer changing\n")
+					continueLHS = False
+					break
+
+			cont = continueLHS | continueRHS
+
+			# print("cont now is " + str(cont))
+
+		return LHS_current, RHS_current
 
 
 
@@ -176,7 +305,7 @@ class ConvexHullSolverThread(QThread):
 		for p in sorted_points:
 			if p.y() > up_y.y():
 				up_y = p
-		self.drawFromTop(sorted_points, up_y)
+		# self.drawFromTop(sorted_points, up_y)
 
 		t2 = time.time()
 		print('Time Elapsed (Sorting): {:3.3f} sec'.format(t2-t1))

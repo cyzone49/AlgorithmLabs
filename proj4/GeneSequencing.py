9f058fa435a8391b8f214bc1733a25459a260b7b
cyzone49 <datvu4749@gmail.com>
2018-11-08 17:19:16 -0700

    added comments to proj4

--- proj4/GeneSequencing.py
+++ proj4/GeneSequencing.py
@@ -35,75 +35,91 @@ class GeneSequencing:
 # you whether you should compute a banded alignment or full alignment, and _align_length_ tells you
 # how many base pairs to use in computing the alignment
 
-	# return min cost of alignment
+    
+    # Unrestricted Approach
+    # param: sequence 1 ( t1 ) and sequence 2 ( t2 )
+    #   len(t1) = n; len(t2) = m
+	# return min cost of alignment using unrestricted approach.
+    # T(n) = O( n * m )    
 	def calcCostUnrestricted( self, t1, t2):
 		if len(t2) > len(t1):
 			temp = t1
 			t1 = t2
 			t2 = temp
 
-		curr = np.zeros( ( len( t1 ) + 1, len( t2 ) + 1 ) )
-		tb_table = np.zeros( ( len( t1 ) + 1, len( t2 ) + 1 ) )
+        # generate operating matrix for the two sequences
+        # longer sequence is vertical (number of rows), the other is horizontal
+		curr = np.zeros( ( len( t1 ) + 1, len( t2 ) + 1 ) ) # O(n * m)
+        
+        # initialize trace table to store direction of each cell
+		tb_table = np.zeros( ( len( t1 ) + 1, len( t2 ) + 1 ) ) # O(n * m)
 
 		# set base column
-		curr[:,0] = np.arange(0, ( len(t1) + 1) * INDEL, INDEL )
+		curr[:,0] = np.arange(0, ( len(t1) + 1) * INDEL, INDEL ) # O( n )
 		# set base row
-		curr[0] = np.arange(0, ( len(t2) + 1) * INDEL, INDEL )
+		curr[0] = np.arange(0, ( len(t2) + 1) * INDEL, INDEL ) # O( m )
 
-		tb_table[:,0] = [0] + [ UP ] * len(t1)
-		tb_table[0] = [0] + [ SIDE ] * len(t2)
+		tb_table[:,0] = [0] + [ UP ] * len(t1) # O( n )
+		tb_table[0] = [0] + [ SIDE ] * len(t2) # O( m )
 
-		# for each row
+        # total: T(n) = O( n * m )
+		# for each row ( n ) excluding first row (base)
 		for r in range(1,curr.shape[0]):
+            # for each column ( m ) excluding first column (base)
 			for c in range(1, curr.shape[1]):
-				if (t1[r-1] == t2[c-1]):
+                
+                # If there is a match!
+				if (t1[r-1] == t2[c-1]):                    
+                    
+                    # set operating matrix and table cells corresponding cell accordingly
 					curr[r][c] = curr[r-1][c-1] + MATCH
 					tb_table[r][c] = DIA
 				else:
+                    # grab min value and the direction in which min value derives
 					min_val, direction_val = self.getMin( curr[r-1][c], curr[r-1][c-1], curr[r][c-1] )
+                    
+                    # set operating matrix and table cells corresponding cell accordingly
 					curr[r][c] = min_val
 					tb_table[r][c] = direction_val
-
+                    
+        # traces the direction of of alignment according to direction stored in trace table tb        
 		dir = self.traceDirection( tb_table )
-
-
+        
+        # generate alignment sequences from alignment dir list
 		seq1 = self.getSeq( t1, dir, False )
 		seq2 = self.getSeq( t2, dir, True )
-		# print("seq1 = " + seq1 )
-		# print("seq2 = " + seq2 )
 
 		return curr[-1,-1], seq1, seq2
 
 
 
-	def cutRow( self, lst ):
-		result = []
-		for item in lst:
-			if item == np.inf:
-				break
-			result.append(item)
-		return np.append( np.full((1, len(lst) - len(result)), np.inf), result )
-
-	def pushRow( self, table, r ):
-		row = self.cutRow( table[r] )
-		table[ r ] = row
-		return table
-
-
+    # Banded Approach
+    # param: t1 = sequence 1,
+    #        t2 = sequence 2
+    # where len(shorter_sequence) = n
+	# return min cost of alignment using banded approach.
+    # T(n) = O( k * n )
 	def calcCostBanded( self, t1, t2):
-		if len(t2) > len(t1):
+        
+        # set shorter -> t1, longer sequence -> t2
+		if len(t2) < len(t1):
 			temp = t1
 			t1 = t2
 			t2 = temp
-		if len(t1)-len(t2)>MAXINDELS:
+            
+        # sequences with significant length discrepancies can not be aligned
+		if len(t1)-len(t2) > MAXINDELS:
 			return np.inf, 'No Alignment Possible.', 'No Alignment Possible.'
-
-		# print("\n\n\n t1 (vertical) = " + t1)
-		# print("\n\n\n t2 (horizontal) = " + t2)
-
-		curr = np.full( ( len(t1) + 1, 2*MAXINDELS+1 ), np.inf )
-		tb_table = np.full( curr.shape, np.inf )
-
+		
+        # generate operating matrix
+        # number of rows = shorter sequence is vertical
+        # number of columns = 2 * MAXINDELS + 1  = 7 = k = bandwidth
+		curr = np.full( ( len(t1) + 1, 2*MAXINDELS+1 ), np.inf ) # O( k * n ) = O( 7n )
+        
+        # generate trace table tb filled with infinity values, saame shape operating matrix
+		tb_table = np.full( curr.shape, np.inf ) 
+        
+        # initialize base columns and rows of operating matrix and trace table
 		base = np.arange(0, ( MAXINDELS + 1 ) * INDEL, INDEL )
 		curr[0] = np.append( base, np.full((1,MAXINDELS), np.inf) )
 		curr[:,0] = np.append( base, np.full((1, len(t1) - MAXINDELS ), np.inf) )
@@ -111,26 +127,51 @@ class GeneSequencing:
 		tb_table[0] = np.append( [0] + [ SIDE ] * MAXINDELS, np.full((1,MAXINDELS), np.inf) )
 		tb_table[:,0] = np.append( [0] + [ UP ] * MAXINDELS, np.full((1, len(t1) - MAXINDELS ), np.inf) )
 
+        # set current row to consider = 1 (1st row -- r=1 -- is base)
 		r = 1
+        
+        # center is the center column index of curr_row
+        # start at 1 (first letter of longer sequence )
+        # end at len(t2) + 1 (at last letter of longer sequence )
 		center = 1
+        
+        # offset indicates number of cell to shift right when calculate curr_row's cells
+        # start at 0 (base row starts from the beginning)
+        # then -> 1 after MAXINDELS + 1 rows to shift each incoming row 1 to the right
 		offset = 0
+        
+        # while still going through each letter of shorter sequence t1 
+        # T(n) = O( n * k )
 		while r <= (len(t1)):
-
-			curr_index = offset # this is current column in curr [0:6]
+            
+            # set starting column/index of curr_row to be the offset value            
+			curr_index = offset 
+            
+            # initialize row of current r ( row_index ) -- size = k
+            # and fill with infinity values
 			curr_row = np.empty( 2 * MAXINDELS + 1 )
 			curr_row.fill(np.inf)
 			tb_row = np.empty( 2 * MAXINDELS + 1 )
 			tb_row.fill(np.inf)
 
-			for c in range( center - MAXINDELS, center + MAXINDELS + 1 ):
-				# c is the current col index of actual table
+            # go through each cell in the k-range == 7 columns,
+            # starting from center-column - MAXINDELS and ending at center-column + MAXINDELS             
+            # c == row_index of the operating matrix that would be generated in unrestricted approach
+            # T(n) = O( k )
+			for c in range( center - MAXINDELS, center + MAXINDELS + 1 ):                
+				
+                # don't consider cases where c is negative
 				if c < 0:
 					pass
+                
+                # current column is the base column. 
+                # Add value in current cell -> curr_row and update trace table
 				elif c == 0:
 					curr_row[curr_index] = curr[r][c]
 					tb_row[curr_index] = UP
 					curr_index += 1
-
+                
+                # when reach end of horizontal sequence (longer) breaks. Finished
 				elif c == ( len(t2) + 1 ):
 					break
 				else:
@@ -138,29 +179,42 @@ class GeneSequencing:
 					if t1[r-1] == t2[c-1]: # letter match
 						curr_row[ curr_index - offset ] = curr[ r - 1 ][ curr_index - 1 ] + MATCH
 						tb_row[ curr_index - offset ] = DIA
-
+                        
+                    # case: corresponding letters from sequences don't match
 					else:
+                        # set upper cell's value to infinity if considering last cell in row
 						if curr_index > 6:
 							up = np.inf
+                        # otherwise consider it normally
 						else:
 							up = curr[ r - 1 ][ curr_index ]
 						diagonal = curr[ r - 1 ][ curr_index - 1 ]
 						left = curr[ r ][ curr_index - offset - 1 ]
-
+                        
+                        # calculate min value of three, then update current cell
+                        # in curr_row and tb_row accordingly
 						min_val, direction_val = self.getMin( up, diagonal, left )
 						curr_row[ curr_index - offset ] = min_val
 						tb_row[ curr_index - offset ] = direction_val
-
+                    
 					curr_index += 1
 					curr[r] = curr_row
 					tb_table[r] = tb_row
-
+            
+            # start shifting 1 cell to the right when reach a full row (size = k)
 			if curr_index > 6:
 				offset = 1
 
 			center += 1
 			r += 1
-
+            
+        # right now, rows of current operating matrix ( k * n ) are either
+        # full (all k cells filled with int values), or
+        # partially filled ( some cells are filled with int, rest are infinity)
+        # where all leftover infinity values are on the right of all applying rows
+        
+        # so we push the last few rows that include infinity values 
+        # so that all int values are on the right side of these rows
 		keep_pushing = True
 		while( keep_pushing ):
 			r -= 1
@@ -169,20 +223,37 @@ class GeneSequencing:
 				tb_table = self.pushRow( tb_table, r )
 			else:
 				keep_pushing = False
-
+                
+        # traces the direction of of alignment according to direction stored in trace table tb        
 		dir = self.traceDirection( tb_table, True )
-		# print("\n\n\n\n\tDone back tracing. dir= " + str(dir))
-		# print("\n\n\tDone back tracing. dir= " + str(dir))
-		# print("\t\t\twith len = " + str(len(dir)))
-
-
+        
+        # generate alignment sequences from alignment dir list
 		seq1 = self.getSeq( t1, dir, False)
 		seq2 = self.getSeq( t2, dir, True)
 
 		return curr[-1,-1], seq1, seq2
+    
+    
+    # generate new list from passed in list so that all infinity values are 
+    # on the left and int values are on the right
+	def cutRow( self, lst ):
+		result = []
+		for item in lst:
+			if item == np.inf:
+				break
+			result.append(item)
+		return np.append( np.full((1, len(lst) - len(result)), np.inf), result )
+    
+    # return a new table where table[r] row is pushed ( from cutRow() )correctly 
+	def pushRow( self, table, r ):
+		row = self.cutRow( table[r] )
+		table[ r ] = row
+		return table
 
 
-
+    # calculate minimum value of the sums of three possible cells and their corresponding costs
+    # param: values of up, diagonal, and left cells of current cell
+    # return: min value and its direction
 	def getMin( self, up, diagonal, left ):
 		min_val = min( [ up + INDEL , diagonal + SUB, left + INDEL ] )
 		if min_val == ( diagonal + SUB ):
@@ -194,47 +265,75 @@ class GeneSequencing:
 
 
 
-
+    # Calculate corresponding alignment of passed in sequence based on direction array
+    # param: seq = sequence,
+    #        dir = direction list, 
+    #        horizontal_axis = position of seq in operating matrix (base column or row)
+    # return: alignment of passed in sequence
+    # T(n) = O( n )
 	def getSeq( self, seq, dir, horizontal_axis=False):
 		# print("in get Seq with seq = " + seq)
 		result = ''
 		index = len(seq)-1
-		for item in dir:
-			# print("\t\tindex=" + str(index))
-			if ( (horizontal_axis == False) & (item == SIDE) ) | ( (horizontal_axis == True) & (item == UP) ):
-				# result += '-'
+        
+        # O(n)
+		for item in dir:			
+            # add insert/deletion '-' to alignment
+			if ( (horizontal_axis == False) & (item == SIDE) ) | ( (horizontal_axis == True) & (item == UP) ):				
 				result = '-' + result
-				# print("\t\tresult = " + result)
-			else:
-				# result += seq[index]
+            # in case of match/subtitution: add letter at current index				
+			else:				
 				result = seq[index] + result
-				index -= 1
-			# print("\t\t=> result = " + result)
+				index -= 1			
 			if index == -1:
 				break
 
 		return result
-
+    
+    
+    # test whether current list contains an infinity value
 	def includeInf( self, lst ):
 		return np.any( lst == np.inf )
 
+    # traces the direction of possible shortest path of alignment
+    # param: tb = trace table
+    #        banded = whether tb is generated from banded algorithm
+    # T(n) = O( n )
 	def traceDirection( self, tb, banded=False ):
 		# print("\n\nGET SEQUENCE starting")
 		# print("\ntb is "+ str(tb.shape) + "\n" + str(tb))
+        
+        # initialize row and column of to be accessed cell in tb
+        # start at last cell and work upwards
 		r = tb.shape[0] - 1
 		c = tb.shape[1] - 1
 		dir = [ tb[ r, c ] ]
 		count = 0
+        
+        # keep going through the cells of trace table until reach first cell ( tb[0][0] )
 		while (r != 0) | (c != 0 ):
 			proceed_normally = True
+            
+            # in case of banded algorithm
 			if banded:
+                # if the row containing current cell also contains and infinity value
+                # or if current does not have infinity but upper row does
+                # then proceed to access trace table normally ( leftup = tb[r-1][c-1], and so on )
+                # This is to account for base rows of banded algorithm where shifting of rows does not occur.
 				if self.includeInf(tb[r]) == True:
 					proceed_normally = True
 				elif ( self.includeInf(tb[r]) == False ) & ( self.includeInf(tb[r-1]) == True ):
 					proceed_normally = True
+                    
+                # else: shift all referenced cells one cell sideway to account 
+                # This is to account for middle rows of banded algorithm where 
+                # shifting of rows does occur ( each row in banded algorithm is 
+                # calculated while is 1 cell shifted to the right based on upper row\,
+                # then is shifted back to fit in 7*n matrix )
 				else:
 					proceed_normally = False
-
+            
+            # calculate corresponding rows and columns while accounting for shifting of rows
 			if proceed_normally == False:
 
 				if   dir[0] == DIA:
@@ -244,7 +343,8 @@ class GeneSequencing:
 				else:
 					r = r - 1
 					c = c + 1
-
+                    
+            # calculate corresponding rows and columns if proceed normally
 			else:
 				if   dir[0] == DIA:
 					r = r - 1
@@ -256,7 +356,9 @@ class GeneSequencing:
 
 			dir.insert(0, tb[r][c] )
 			count += 1
-
+        
+        # reverse finished direction list to start from the [0][0] cell
+        # to calculate alignment sequences
 		dir.reverse()
 		return dir
 
@@ -267,21 +369,6 @@ class GeneSequencing:
 		self.MaxCharactersToAlign = align_length
 		results = []
 
-		# self.calcCostBanded('cataca', 'cotaca')
-
-
-		# if banded == False:
-		# 	score, s1, s2 = self.calcCostUnrestricted(sequences[0], sequences[1])
-		# else:
-		# score, s1, s2 = self.calcCostBanded(sequences[8][0:align_length], sequences[9][0:align_length])
-		# print("s1 = " + s1)
-		# print("s2 = " + s2)
-		# #
-		# print("\n\n\n\nlen s1 = " + str(len(s1)))
-		# print("\nlen s2 = " + str(len(s2)))
-		# print("score= " + str(score))
-
-
 		for i in range(len(sequences)):
 			jresults = []
 			for j in range(len(sequences)):
@@ -291,26 +378,14 @@ class GeneSequencing:
 				else:
 
 					if banded:
-						score, alignment1, alignment2 = self.calcCostBanded( sequences[i][0:align_length], sequences[j][0:align_length] )
-						# print("\nalignment1[0:100] = " + alignment1[0:100])
-						# print("\n\t\twith len = " + str(len(alignment1[0:100])))
-						#
-						# print("\nalignment2[0:100] = " + alignment2[0:100])
-						# print("\n\t\twith len = " + str(len(alignment2[0:100])))
+						score, alignment1, alignment2 = self.calcCostBanded( sequences[i][0:align_length], sequences[j][0:align_length] )						
 					else:
 						score, alignment1, alignment2 = self.calcCostUnrestricted( sequences[i][0:align_length], sequences[j][0:align_length] )
-
-					# alignment1 = seq1 + '  DEBUG:(seq{}, {} chars,align_len={}{})'.format(i+1,
-					# 	len(sequences[i]), align_length, ',BANDED' if banded else '')
-					# alignment2 = seq2 + '  DEBUG:(seq{}, {} chars,align_len={}{})'.format(j+1,
-					# 	len(sequences[j]), align_length, ',BANDED' if banded else '')
 
 					s = {'align_cost':score, 'seqi_first100':alignment1[0:100], 'seqj_first100':alignment2[0:100]}
 					table.item(i,j).setText('{}'.format(int(score) if score != math.inf else score))
 					table.repaint()
 				jresults.append(s)
 			results.append(jresults)
-
-		# print("RESULT = ")
-		# print(str(jresults))
+            
 		return results
